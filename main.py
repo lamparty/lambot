f11feb23de51b9268cbf36b00fcd339c028d8a7c
Toombez <TimurTokaevToomber@gmail.com>
2021-08-09 02:43:33 +0300

    on_member_join add guest role

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from discord.utils import get
 
 intents = discord.Intents.default()
 intents.members = True
@@ -24,23 +25,27 @@ async def on_ready():
 	print(f'ready on "{lampartyGuild}" guild.')
 	#create channels for guest members
 	global workCategory
-	workCategory = await lampartyGuild.create_category('for lambot')
+	workCategory = await lampartyGuild.create_category('Анкеты')
+	global guestRole 
+	guestRole = discord.utils.get(lampartyGuild.roles, name = "гость")
 	#get users from db and create guest member channels
 	pass
 
 @bot.event
 async def on_member_join(member):
 	#check user is guest or not
-	#await member.add_roles('гость')
+	await member.add_roles(guestRole)
 	await workCategory.create_text_channel(member.name)
 	#creating guest channel on join and adding user to db
 	pass
 
 @bot.command()
-async def turn_off(ctx):
+async def turnOff(ctx):
 	for channel in workCategory.channels:
 		await channel.delete()
 	await workCategory.delete()
 	pass
 
+async def giveRole():
+	pass
 bot.run('ODY4NjIxMDg2NjEzNDU5MDEz.YPyUbQ._KAVTEqDSJ7l0Mtm1delxSZI4bI' )

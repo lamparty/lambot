02cb4714bd40ca9cfbff472b1e45693a30472c7b
Toombez <TimurTokaevToomber@gmail.com>
2021-08-09 04:20:03 +0300

    delete user class, connect to mongo

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+import pymongo
 from discord.ext import commands
 from discord.utils import get
 
@@ -6,29 +7,24 @@ intents = discord.Intents.default()
 intents.members = True
 bot = commands.Bot(command_prefix='!',intents=intents)
 
-class user:
-	def __init__(self, discordID):
-		self.id = discordID
-		pass
-	
-	def insertIntoBD(self, collection):  # using with "import pymongo" | put inside user class
-		data = {
-			f'{self.discordID}': f'{self.discordID}',
-			f'{self.mojangUUID}': f'{self.mojangUUID}'
-		}
-		return collection.insert_one(data)
+mongoClient = pymongo.MongoClient('localhost', 27017)
+lampartyDB = mongoClient.lamparty
+registredUsersCollection = lampartyDB.registred_users
+registredUsers = registredUsersCollection.find()
 
 @bot.event
 async def on_ready():
 	global lampartyGuild 
 	lampartyGuild = bot.get_guild(727245965345685514)
-	print(f'ready on "{lampartyGuild}" guild.')
-	#create channels for guest members
+	
 	global workCategory
 	workCategory = await lampartyGuild.create_category('Анкеты')
 	
 	global guestRole 
 	guestRole = discord.utils.get(lampartyGuild.roles, name = "гость")
+	
+	print(f'ready on "{lampartyGuild}" guild.')
+	#create channels for guest members
 	await workCategory.set_permissions(guestRole, read_messages = True)
 	await workCategory.set_permissions(bot.user, read_messages = True)
 	await workCategory.set_permissions(lampartyGuild.default_role, read_messages = False)
@@ -43,6 +39,7 @@ async def on_member_join(member):
 	await memberChannel.set_permissions(guestRole, read_messages = False)
 	await memberChannel.set_permissions(member, read_messages = True)
 	await memberChannel.send('Hello')
+	insertIntoBD(member.id, registredUsersCollection)
 	#creating guest channel on join and adding user to db
 	pass
 
@@ -52,5 +49,11 @@ async def deleteQuestionnaires(ctx):
 		await channel.delete()
 	await workCategory.delete()
 	pass
+
+def insertIntoBD(discordID, collection):
+	data = {
+		'discordID': f'{discordID}'
+	}
+	return collection.insert_one(data)
 
 bot.run('ODY4NjIxMDg2NjEzNDU5MDEz.YPyUbQ._KAVTEqDSJ7l0Mtm1delxSZI4bI' )

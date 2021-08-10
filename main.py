7a397ea55efd8ddc16b412ac4463aa87bf1fc585
Toombez <TimurTokaevToomber@gmail.com>
2021-08-10 04:07:19 +0300

    refactoring

--- main.py
+++ main.py
@@ -3,61 +3,67 @@ import pymongo
 from discord.ext import commands
 from discord.utils import get
 
+#setup bot
 intents = discord.Intents.default()
 intents.members = True
 bot = commands.Bot(command_prefix='!',intents=intents)
 
+#connection to db
 mongoClient = pymongo.MongoClient('localhost', 27017)
 lampartyDB = mongoClient.lamparty
 registredUsersCollection = lampartyDB.registred_users
+
 registredUsers = registredUsersCollection.find()
 
 @bot.event
 async def on_ready():
-	global lampartyGuild 
+	#getting guild
+	global lampartyGuild
 	lampartyGuild = bot.get_guild(727245965345685514)
-	
-	global workCategory
-	workCategory = await lampartyGuild.create_category('Анкеты')
-	
-	global guestRole 
+
+	#getting roles
+	global guestRole
+	global playerRole
 	guestRole = discord.utils.get(lampartyGuild.roles, name = 'гость')
-	
-	print(f'ready on "{lampartyGuild}" guild.')
-	#create channels for guest members
-	await workCategory.set_permissions(guestRole, read_messages = True)
-	await workCategory.set_permissions(bot.user, read_messages = True)
-	await workCategory.set_permissions(lampartyGuild.default_role, read_messages = False)
-	#get users from db and create guest member channels
-	pass
+	playerRole = discord.utils.get(lampartyGuild.roles, name = 'йухный ауфер') # change on 'игрок'
+
+	#create categoryChannel for guests only
+	global questionnairesCategory
+	questionnairesCategory = await lampartyGuild.create_category('Анкеты')
+
+	await questionnairesCategory.set_permissions(guestRole, read_messages = True)
+	await questionnairesCategory.set_permissions(bot.user, read_messages = True)
+	await questionnairesCategory.set_permissions(lampartyGuild.default_role, read_messages = False)
+
+	return print(f'{bot.user.name} ready on "{lampartyGuild}" guild.')
 
 @bot.event
 async def on_member_join(member):
-	#check user is guest or not
-	if (not availableInDB(member.id)):
-		await member.add_roles(guestRole)
-		#creating guest channel on join and adding user to db
-		memberChannel = await workCategory.create_text_channel(member.name, sync_permissions=False)
+	if (is_registred(member.id)):
+		await member.add_roles(playerRole)
+	else:
+		#creating memberChannel
+		memberChannel = await questionnairesCategory.create_text_channel(member.name, sync_permissions=False)
+
 		await memberChannel.set_permissions(guestRole, read_messages = False)
 		await memberChannel.set_permissions(member, read_messages = True)
 		await memberChannel.send('Hello')
-		#insertIntoDB(member.id, registredUsersCollection) # move into 'add_on_server' function
-	else:
-		userRole = discord.utils.get(lampartyGuild.roles, name = 'йухный ауфер') # change on 'игрок'
-		await member.add_roles(userRole)
+
+		await member.add_roles(guestRole)
 	pass
+
 @bot.event
 async def on_member_remove(member):
-	if (not availableInDB(member.id)):
-		memberChannel = discord.utils.get(workCategory.text_channels, name = member.name)
+	if (not is_registred(member)):
+		memberChannel = discord.utils.get(questionnairesCategory.text_channels, name = member.name)
 		await memberChannel.delete()
 	pass
 
 @bot.command()
-async def deleteQuestionnaires(ctx):
-	for channel in workCategory.channels:
+async def deleteQ(ctx):
+	for channel in questionnairesCategory.channels:
 		await channel.delete()
-	await workCategory.delete()
+	await questionnairesCategory.delete()
 	pass
 
 def insertIntoDB(discordID, collection):
@@ -68,10 +74,9 @@ def insertIntoDB(discordID, collection):
 	registredUsers = registredUsersCollection.find()
 	return collection.insert_one(data)
 
-def availableInDB(id):
+def is_registred(discordUser):
 	for user in registredUsers:
-		print(str(id) == user['discordID'])
-		if (user['discordID'] == str(id)):
+		if (str(discordUser.id) == str(user['discordID'])):
 			return True
 	return False
 

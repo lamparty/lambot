140f3e155724f5657bc4bc841c22b62520322e8c
Toombez <TimurTokaevToomber@gmail.com>
2021-08-22 13:38:34 +0300

    register func and it's call

--- main.py
+++ main.py
@@ -31,13 +31,20 @@ questions = [
 	, "Был ли у вас ранее опыт игры на подобных серверах? (Если да, то почему ушли?)"
 	, "Ознакомились ли вы с правилами сервера?"
 	, "Кого нужно брать с собой на вечеринку?"
-	, "Спасибо за заполнение анкеты"
+	, "Ваша анкета отправлена на рассмотрение. Анкеты рассматриваются до 12 часов с момента подачи."
 ]
 
 @bot.event
 async def on_ready():
 	def findFormCategory(guild):
 		return discord.utils.get(guild.categories, name = "Анкеты")
+
+	def getAllGuest(guild):
+		guests = []
+		for user in guild.members:
+			if playerRole not in  user.roles and not user.bot:
+				guests.append(user)
+		return guests
 
 	def getGuildAndRole():
 		global lampartyGuild
@@ -63,14 +70,13 @@ async def on_ready():
 		await formsCategory.set_permissions(lampartyGuild.default_role, read_messages = True)
 		
 		pass
-
-
+	
 	getGuildAndRole()
 	await createFormCategory()
 	
 	guests = getAllGuest(lampartyGuild)
 	for user in guests:
-		await createMemberChannel(user)
+		channel = await createMemberChannel(user)
 
 	return print(f'{bot.user.name} ready on "{lampartyGuild}" guild.')
 
@@ -85,53 +91,65 @@ async def on_member_join(member):
 
 @bot.event
 async def on_reaction_add(reaction, user):
-	print(reaction, user)
+	#print(reaction, user)
+	if (reaction.emoji == "\N{Llama}") and (reaction.message.channel.category == formsCategory) and (not user.bot) and (reaction.message.content == reactionPhrase):
+		await reaction.message.remove_reaction("\N{Llama}", bot.user)
+		await reaction.message.delete()
+		await register(user)
 	pass
 
 async def register(member):
-	#sendAllAnswers()
+	async def clearForm(channel):
+		async for message in channel.history():
+			if (not message.content == helloPhrase):
+				await message.delete()
+		pass
+	
+	async def form(channel):
+		def memberChannelCheck(m):
+			print(m.channel in formsCategory.channels)
+			#change check
+			return (m.channel in formsCategory.channels) and (not m.author.bot)
+		async def deleteReactionFromPrevMessage(channel):
+			async for message in channel.history():
+				if message.author.bot:
+					return await message.clear_reaction("\N{Llama}")
+
+		#asking questions
+		botMessage = await channel.send(questions[0])
+		await botMessage.add_reaction("\N{Llama}")
+		await memberChannel.set_permissions(member, send_messages = True, read_messages = True)
+		for questionID in range(1, len(questions)):
+			try:
+				userMessage = await bot.wait_for('message', timeout = 120.0, check = memberChannelCheck)
+			except asyncio.TimeoutError:
+				await memberChannel.set_permissions(member, send_messages = False, read_messages = True)
+				await deleteReactionFromPrevMessage(channel)
+				await waitReactionOnPhrase(channel, "Время для ответа на вопрос закончилось. Для заполнения анкеты нажмите на реакцию ниже.")
+				#await clearForm(channel)
+				return False
+			else:
+				await deleteReactionFromPrevMessage(channel)
+				botMessage = await channel.send(questions[questionID])
+				await botMessage.add_reaction("\N{Llama}")
+				if (questionID == len(questions) - 1):
+					return True
+
+	registred = False
+	memberChannel = discord.utils.find(lambda c: member in c.members, formsCategory.channels)
+	
+	while not registred:
+		registred = await form(memberChannel)
+	
+	await memberChannel.set_permissions(member, send_messages = False, read_messages = True)
 	pass
-
-@bot.command()
-async def delForms(ctx):
-	for channel in formsCategory.channels:
-		await channel.delete()
-	await formsCategory.delete()
-	pass
-
-# def insertIntoDB(discordUser, collection):
-# 	data = {
-# 		'discordID': discordUser.id
-# 	}
-# 	return collection.insert_one(data)
-
-def is_registred(discordUser):
-	global registredUsers
-	registredUsers = registredUsersCollection.find()
-	for user in registredUsers:
-		if (discordUser.id == user["discordID"]):
-			return True
-	return False
-
-# async def add_to_server(discordUser):
-# 	#inserting into DB, add to whitelist on two servers, change role, nick and delete memberChannel
-# 	insertIntoDB(discordUser)
-# 	#lampartyCreativeResponce = await rcon(f'whitelist add {discordUser.nick}', host='135.181.126.191', port=25658, passwd='bF52JuRi')
-# 	#lampartyResonce = await rcon(f'whitelist add {discordUser.nick}', host='95.216.92.76', port=25861, passwd='1O4CnTkm')
-
-# async def clearForm(channel):
-# 	messages = channel.history()
-# 	async for message in messages:
-# 		#if (not message.content == helloPhrase):
-# 		await message.delete()
-# 	pass
 
 async def waitReactionOnPhrase(channel, phrase):
 	message = await channel.send(phrase)
 	await message.add_reaction("\N{Llama}")
 
 	def llamaEmojiCheck(reaction, user):
-		return (user.name == channel.name) and (reaction.emoji == "\N{Llama}") and (message.content == phrase)
+		return (user in reaction.message.channel.members) and (reaction.emoji == "\N{Llama}") and (message.content == phrase) and (not user.bot)
 
 	reaction, user = await bot.wait_for("reaction_add", check = llamaEmojiCheck)
 	pass
@@ -146,43 +164,7 @@ async def createMemberChannel(member):
 	message = await channel.send(reactionPhrase)
 	await message.add_reaction("\N{Llama}")
 	
-	print(member in message.channel.members)
 	return channel
-# async def form(channel):
-# 	def memberChannelCheck(m):
-# 		return m.author.name == channel.name and m.guild
-	
-# 	await channel.set_permissions(lampartyGuild.default_role, send_messages = True)
-	
-# 	time.sleep(10)
-# 	formFinished = False
-	
-# 	while (not formFinished):
-# 		await channel.send(questions[0])	
-# 		for questionID in range(1, len(questions)):
-# 			try:
-# 				msg = await bot.wait_for('message', timeout = 20.0, check = memberChannelCheck)
-# 			except asyncio.TimeoutError:
-# 				await waitReactionOnPhrase(channel, "Повторить")
-# 				print("jgak;waf")
-# 				await clearForm(channel)
-# 				break
-# 			else:
-# 				#check about cancel function
-# 				await channel.send(questions[questionID])
-# 				if (questionID == len(questions) - 1):
-# 					formFinished = True
-
-# 		#await bot.wait_for("message", timeout = 120.0, check=check)
-		
-# 	await channel.set_permissions(lampartyGuild.default_role, send_messages = False)
-
-def getAllGuest(guild):
-	guests = []
-	for user in guild.members:
-		if playerRole not in  user.roles and not user.bot:
-			guests.append(user)
-	return guests
 
 @bot.event
 async def on_member_remove(member):
@@ -190,5 +172,13 @@ async def on_member_remove(member):
 		memberChannel = discord.utils.get(formsCategory.text_channels, name = member.name)
 		await memberChannel.delete()
 	pass
+
+def is_registred(discordUser):
+	global registredUsers
+	registredUsers = registredUsersCollection.find()
+	for user in registredUsers:
+		if (discordUser.id == user["discordID"]):
+			return True
+	return False
 
 bot.run("ODY4NjIxMDg2NjEzNDU5MDEz.YPyUbQ._KAVTEqDSJ7l0Mtm1delxSZI4bI")

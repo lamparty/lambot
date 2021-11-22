0bf4cd7b2449f6c1463f9ad78d9712cd91d07845
toombez <timurtokaev.toombez@gmail.com>
2021-11-22 08:52:39 +0300

    loading all modules on ready client

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from discord.ext import commands
 import json
+import os
 
 with open("settings.json") as f:
     settings = json.load(f)
 prefix = settings["bot"]["prefix"]
 token = settings["bot"]["token"]
+modules_path = settings["bot"]["modules_path"]
 
 client = commands.Bot(command_prefix=prefix)
-client.load_extension(f"{settings['bot']['modules_path']}.ModulesManager")
+
+@client.event
+async def on_ready():
+    for module in os.listdir(modules_path):
+        if (module.endswith(".py")):
+            client.load_extension(f"{modules_path}.{module[:-3]}")
+
 client.run(token)
